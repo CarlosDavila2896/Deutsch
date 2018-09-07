34af8e7b9a600cedb6734bb1ca8e306ffe73ead4
Carlos Davila <carlos.davila.armijos@udla.edu.ec>
2018-09-07 14:11:58 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+from wort import Wort
+import random
+
+JUGAR = 1
+AGREGAR_PALABRAS = 2
+
+global f
+global m
+global wortList
+
+def main():
+    global m
+    while True:
+        print("\n*****Menu*****")
+        print("1. Jugar")
+        print("2. Agregar palabras")
+        print("3. Salir")
+        m = int(input("Seleccione una: "))
+        if(m > 0 and m < 4):
+            break
+    
+    
+     
+    if(m == 3):
+        print("Tschüs!!!")
+        return
+
+    cargarPalabras()    
+    if(m == 1):
+        juego()
+        main()
+
+    if(m == 2):
+        agregarPalabras()
+        main()
+
+
+def cargarPalabras():
+    global wortList
+    global f
+    wortList = []
+    with open('wortebuch.txt', "r", encoding='utf-8') as f:
+        for line in f:
+            artikel = line.split()
+            wort = line.split()
+            w = Wort(artikel,wort)
+            wortList += [w]
+
+    if m == AGREGAR_PALABRAS:
+        f = open("wortebuch.txt", "a", encoding='utf-8')
+
+
+def agregarPalabras():
+    global f
+    global wortList
+    while True:
+        wort = input("Ingrese una palabra: ")
+        if(wort == "0"):
+            f.close()
+            return
+        while True:
+            artikel = input("Ingrese su articulo: ")
+            if(artikel == "0"):
+                f.close()
+                return
+            w = Wort(artikel,wort)
+            if(w.checkArtikel()):
+                break
+            else:
+                print("Artículo no valido")
+        if (not w in wortList):
+            f.write(str(w)+"\n")
+        else:
+            print("Esa palabra ya existe!")
+
+
+def juego():
+    global wortList
+    while True:        
+        w = wortList[random.randrange(0,len(wortList))]
+        resp = input("Cual es el articulo de " + w.wort.getWort() + ": ")
+        if(resp == "0"):
+            return
+        if(resp == w.getArtikel()):
+            print("Richtig!!")
+        else:
+            print("Nein! El artículo correcto es: " + w.wort.getArtikel())
+        
+
+if __name__ == "__main__":
+    main()
---
+++ wort.py
@@ -0,0 +1,30 @@
+ARTIKELS = ['der', 'die', 'das']
+
+class Wort:
+    artikel = ''
+    wort = ''
+
+    def __init__(self, artikel, wort):
+        self.artikel = artikel
+        self.wort = wort
+
+    def __str__(self):
+        return self.artikel + ' ' + self.wort
+
+    def checkArtikel(self):
+        if (self.artikel in ARTIKELS):
+            return True
+        else:
+            return False
+
+    def checkWort(self):
+        if(len(self.wort.strip())>1):
+            return True
+        else:
+            return False
+
+    def getWort(self):
+        return self.wort
+
+    def getArtikel(self):
+        return self.artikel
